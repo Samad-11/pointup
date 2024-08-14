ac007424a70b27404c3e61ded549360b42fda953
Abdus Samad <120767334+Samad-11@users.noreply.github.com>
2024-08-14 15:01:00 +0530

    Update ApplyButton.tsx

--- src/components/ui/ApplyButton.tsx
+++ src/components/ui/ApplyButton.tsx
@@ -8,7 +8,7 @@ import {useRouter} from 'next/navigation'
 const ApplyButton = ({ buttonText }: { buttonText: String }) => {
   const {push} = useRouter()
     return (
-        <a href=`https://wa.me/${WHATSAPP_NUMBER}?text=${WHATSAPP_MESSAGE}`
+        <a href={`https://wa.me/${WHATSAPP_NUMBER}?text=${WHATSAPP_MESSAGE}`}
             
             className="border font-medium relative border-white/[0.2] text-white px-10 py-1 rounded-full flex justify-center items-center gap-3 max-w-full">
             <span className='text-nowrap max-sm:text-xs'>{buttonText}</span>
