be205e09dc310c8f1dfb16995f4afedeab087f08
Abdus Samad <120767334+Samad-11@users.noreply.github.com>
2024-08-14 14:40:48 +0530

    Update ApplyButton.tsx link to div

--- src/components/ui/ApplyButton.tsx
+++ src/components/ui/ApplyButton.tsx
@@ -1,21 +1,21 @@
-
+'use client'
 import { WHATSAPP_MESSAGE, WHATSAPP_NUMBER } from '@/lib/constants'
 import Link from 'next/link'
 import React from 'react'
 import { FaLongArrowAltRight } from 'react-icons/fa'
+import {useRouter} from 'next-navigation'
 
 const ApplyButton = ({ buttonText }: { buttonText: String }) => {
-  
+  const {push} = useRouter()
     return (
-        <Link
-            target='_blank'
-            href={`https://wa.me/${WHATSAPP_NUMBER}?text=${WHATSAPP_MESSAGE}`}
+        <div
+            onClick={()=>push(`https://wa.me/${WHATSAPP_NUMBER}?text=${WHATSAPP_MESSAGE}`)}
             
             className="border font-medium relative border-white/[0.2] text-white px-10 py-1 rounded-full flex justify-center items-center gap-3 max-w-full">
             <span className='text-nowrap max-sm:text-xs'>{buttonText}</span>
             <span><FaLongArrowAltRight className='text-2xl' /></span>
             <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-[#d1ab38] to-transparent  h-px" />
-        </Link>
+        </div>
     )
 }
 
