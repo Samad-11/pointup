5186cffa614fa496664c6c289e24661fdff6768b
Abdus Samad <120767334+Samad-11@users.noreply.github.com>
2024-08-14 14:55:54 +0530

    Update ApplyButton.tsx div to a

--- src/components/ui/ApplyButton.tsx
+++ src/components/ui/ApplyButton.tsx
@@ -8,14 +8,14 @@ import {useRouter} from 'next/navigation'
 const ApplyButton = ({ buttonText }: { buttonText: String }) => {
   const {push} = useRouter()
     return (
-        <div
-            onClick={()=>push(`https://wa.me/${WHATSAPP_NUMBER}?text=${WHATSAPP_MESSAGE}`)}
+        <a
+           href=`https://wa.me/${WHATSAPP_NUMBER}?text=${WHATSAPP_MESSAGE}`
             
             className="border font-medium relative border-white/[0.2] text-white px-10 py-1 rounded-full flex justify-center items-center gap-3 max-w-full">
             <span className='text-nowrap max-sm:text-xs'>{buttonText}</span>
             <span><FaLongArrowAltRight className='text-2xl' /></span>
             <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-[#d1ab38] to-transparent  h-px" />
-        </div>
+        </a>
     )
 }
 
